25819b60ee1369ee37bb31bbfb548149ace519bd
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2025-11-17 08:57:17 +0100

    jkh

--- normalizza/Program.cs
+++ normalizza/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace normalizza
+﻿using System.Runtime.Intrinsics;
+
+namespace normalizza
 {
     internal class Program
     {
+        static double[] normalizza(int[] v1)
+        {
+            double[] v2 = new double[v1.Length];
+
+            for(int i = 0; i < v1.Length; i++)
+            {
+                if(i == 0)
+                {
+                    v2[i] = (v1[i] + v1[i] + v1[i + 1]) / 3; 
+                }
+                else if(i == v1.Length)
+                {
+                    v2[i] = (v1[i] + v1[i] + v1[i - 1]) / 3;
+                }
+                else
+                {
+                    v2[i] = (v1[i] + v1[i - 1] + v1[i + 1]) / 3;
+                }
+            }
+
+            Console.Write("Il secondo vettore è " + v2);
+            return v2;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] v1 = { 4, 5, 2, 8, 5 };
+
+            normalizza(v1);
         }
     }
 }
